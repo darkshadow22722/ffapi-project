4e73b3a1f3f5ff0e844f42059005f0a735517a36
Giacomo Ferretti <me@hexile.xyz>
2019-09-11 18:26:43 +0200

    Fixed promocode generation

--- ffapi/ffapi/mcd/it/promo.py
+++ ffapi/ffapi/mcd/it/promo.py
@@ -18,22 +18,24 @@ import json
 import logging
 import os
 import random
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import requests
 
 from ffapi import utils
+
+_logger = logging.getLogger(__name__)
 
 check_invoice = 'https://mcdonalds.fast-insight.com/voc/bs/api/v3/it/checkInvoice'
 
 # Load locations
 _locations_filename = 'locations.json'
 if os.path.isfile(os.path.join(os.path.dirname(__file__), _locations_filename)):
-    # __logger__.debug('Loading "{}"...'.format(__locations_filename))
+    _logger.debug('Loading "{}"...'.format(_locations_filename))
     with open(os.path.join(os.path.dirname(__file__), _locations_filename)) as f:
         locations = json.loads(f.read())
 else:
-    # __logger__.warning('"{}" not found. Most promocodes won\'t be valid.'.format(__locations_filename))
+    _logger.debug('"{}" not found. Most promocodes won\'t be valid.'.format(_locations_filename))
     pass
 
 
@@ -45,6 +47,10 @@ def days_diff(d1):
     d1 = datetime.strptime(d1, "%Y-%m-%d")
     d2 = datetime.strptime('2014-12-31', "%Y-%m-%d")
     return abs((d2 - d1).days)
+
+
+def days_to_date(d):
+    return datetime.strptime('2014-12-31', "%Y-%m-%d") + timedelta(days=int(d))
 
 
 def flip(string):
@@ -83,8 +89,36 @@ def checksum(src):
     return result
 
 
+def verify_checksum(src):
+    c_checksum = get_checksum(src)
+    c_no_checksum = get_code_without_checksum(src)
+
+    return c_checksum == c_no_checksum
+
+
 def insert_at(src, ins, ind):
     return src[:ind] + ins + src[ind:]
+
+
+def get_checksum(src):
+    return src[11:12] + src[7:8] + src[1:2]
+
+
+def get_code_without_checksum(src):
+    return src[:1] + src[2:7] + src[8:11] + src[12:]
+
+
+def decode_promocode(promocode):
+    gen = get_code_without_checksum(promocode)
+    gen2 = str(base28_to_decimal(gen))[1:]
+    gen3 = flip(gen2)
+
+    return {
+        'pos': gen3[4:6],
+        'site': gen3[:4],
+        'date': days_to_date(gen3[6:10]),
+        'num': gen3[10:]
+    }
 
 
 def generate_promocode(pos, site, date, num_trans):
@@ -103,12 +137,12 @@ def generate_promocode(pos, site, date, num_trans):
 
 def generate_random_promocode():
     if 'locations' in globals():
-        location = int(random.choice(locations))
+        location = random.choice(locations)
     else:
         location = random.randint(1, 3240)
 
     data = {
-        'pos': random.randint(0, 99),
+        'pos': random.randint(0, 20),
         'site': location,
         'date': parse_date(datetime.now()),
         'num_trans': random.randint(0, 9999)
@@ -123,7 +157,11 @@ def generate_survey_link(promocode, proxies=None, verify=True):
         'meta': {}
     }
 
+    _logger.debug(decode_promocode(promocode))
+
     x = requests.post(check_invoice, json=data, proxies=proxies, verify=verify)
+
+    _logger.debug('{}: {}'.format(x.status_code, x.content))
 
     if x.status_code == 200:
         y = json.loads(x.content.decode())
