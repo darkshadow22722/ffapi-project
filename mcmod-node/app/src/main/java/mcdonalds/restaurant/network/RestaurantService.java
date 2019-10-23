5c95f564dbb5e0860779cc2cec076b2799e79868
Giacomo Ferretti <me@hexile.xyz>
2019-10-24 01:09:13 +0200

    Removed unused imports

--- mcmod-node/app/src/main/java/mcdonalds/restaurant/network/RestaurantService.java
+++ mcmod-node/app/src/main/java/mcdonalds/restaurant/network/RestaurantService.java
@@ -9,8 +9,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
-import android.os.AsyncTask;
-import android.os.Binder;
 import android.os.Build;
 import android.os.IBinder;
 import android.util.Base64;
